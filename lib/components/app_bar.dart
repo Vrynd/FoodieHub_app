c7f91f17b532c848b90a7b0693d2b7f021fd7840
Vrynd <vryndhika@gmail.com>
2025-09-01 19:51:24 +0700

    Menambakan widget action didalam appbar

--- lib/components/app_bar.dart
+++ lib/components/app_bar.dart
@@ -4,12 +4,13 @@ class AppBarTemplate extends StatelessWidget implements PreferredSizeWidget {
   final Widget title;
   final Widget? leading;
   final bool? centerTitle;
-
+  final List<Widget>? action;
   const AppBarTemplate({
     super.key,
     this.leading,
     this.centerTitle,
     required this.title,
+    this.action,
   });
 
   @override
@@ -20,6 +21,7 @@ class AppBarTemplate extends StatelessWidget implements PreferredSizeWidget {
       shadowColor: Theme.of(context).colorScheme.shadow,
       backgroundColor: Theme.of(context).colorScheme.onPrimary,
       leading: leading,
+      actions: action,
       centerTitle: centerTitle,
       title: title,
     );
